32a0f20fdf63d4ef90a9d7bbb9ce5a67f89310b1
concurrent-server-0630 <andycruz008@gmail.com>
2020-01-22 14:42:04 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -1,4 +1,4 @@
-# Helper Methods
+ # Helper Methods
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -36,3 +36,7 @@ def turn(board)
 end
 
 # Define your play method below
+def play(board)
+   9.times do 
+    turn()
+end
